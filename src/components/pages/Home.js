10794d8c62bca5e9a126718fc13d6a0081322673
ofkTuhin <tuhin.omar99@gmail.com>
2022-08-04 16:07:56 +0600

    add date

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Moment from "react-moment";
 
 import Videos from "../Videos";
 
 const Home = () => {
-  return <Videos />;
+  return (
+    <>
+      <Moment toNow>{new Date("1976-04-19T12:59-0500")}</Moment>
+      <Videos />
+    </>
+  );
 };
 
 export default Home;
